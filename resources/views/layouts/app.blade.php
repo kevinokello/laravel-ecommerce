72cd0f70346ca15481c9e088bf71abf01d5cb005
kevinokello <okellokevin77@gmail.com>
2022-11-07 16:26:57 +0300

    login layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,7 +79,7 @@
                 <div class="container">
                     <div class="header-wrap">
                         <div class="logo logo-width-1">
-                            <a href="index.html"><img src="assets/imgs/logo/logo.png" alt="logo"></a>
+                            <a href="{{ url('/') }}"><img src="assets/imgs/logo/logo.png" alt="logo"></a>
                         </div>
                         <div class="header-right">
                             <div class="search-style-1">
