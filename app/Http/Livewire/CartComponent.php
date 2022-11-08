07475193edb073e4113bda69a13b0cfd6111b78c
kevinokello <okellokevin77@gmail.com>
2022-11-08 10:17:05 +0300

    dynamic cart icon

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -12,21 +12,25 @@ class CartComponent extends Component
         $product = Cart::get($rowId);
         $qty = $product->qty +1;
         Cart::update($rowId,$qty);
+        $this->emitTo('cart-icon-component','refreshComponent');
     }
     public function decreaseQuantity($rowId)
     {
         $product = Cart::get($rowId);
         $qty = $product->qty - 1;
         Cart::update($rowId, $qty);
+        $this->emitTo('cart-icon-component', 'refreshComponent');
     }
     public function destroy($id)
     {
         Cart::remove($id);
+        $this->emitTo('cart-icon-component', 'refreshComponent');
         session()->flash('success_message','Item removed succesfully');
     }
     public function clearAll()
     {
         Cart::destroy();
+        $this->emitTo('cart-icon-component', 'refreshComponent');
     }
     public function render()
     {
