50ad3d04f6331b1904f66c79627a3e6ffb18f783
kevinokello <okellokevin77@gmail.com>
2022-11-09 09:36:48 +0300

    responsive search

---
+++ resources/views/livewire/header-search-component.blade.php
@@ -0,0 +1,6 @@
+    <div class="search-style-1">
+        <form action="#">
+            <input type="text" name="q" placeholder="Search for items...">
+            <button type="submit"><i class="fi-rs-search"></i></button>
+        </form>
+    </div>
